d10b9af181f6d7b420cfbdfcad4ac388c3e6674d
Akira Tamamori <tamamori5917@gmail.com>
2023-08-25 14:44:15 +0900

    Update inference.py

--- clustering/gaussian/inference.py
+++ clustering/gaussian/inference.py
@@ -21,8 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import warnings
-
 from hydra import compose, initialize
 from omegaconf import DictConfig, OmegaConf
 from progressbar import progressbar as prg
@@ -31,8 +29,6 @@ from dataset import get_dataset
 from model import get_model
 from util import (append_stats, calc_accuracy, get_device, init_manual_seed,
                   init_stats, load_checkpoint, print_stats)
-
-warnings.simplefilter("ignore", UserWarning)
 
 
 def main(cfg: DictConfig):
